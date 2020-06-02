e8dd3dd883bfcaf22ceea19e8c85b79473a53580
Spencer Moody <spencer.moody@outlook.com>
2020-06-02 13:41:33 -0700

    Add jumbotron, container and more terms/answers
    Co-authored-by: Taylor Phillips <taylorphillips133@gmail>

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
   $("#javaScript").click(function() {
     $("#javaScript-answer").fadeToggle();
   });
+  $("#operators").click(function() {
+    $("#operators-answer").fadeToggle();
+  });
+  $("#variables").click(function() {
+    $("#variables-answer").fadeToggle();
+  });
 });
